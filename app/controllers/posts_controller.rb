448393a407fc9f7b94552756cf170cc2e58bab6b
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-05-02 19:28:14 +0900

    destroyまで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,43 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @new_posts = Post.all
+    @author = Author.first
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body, :category)
 end
