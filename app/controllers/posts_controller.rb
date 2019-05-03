43a76c2b4b919983caff4f8cd6187faea93ef07e
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-05-03 14:22:07 +0900

    ページネーションとscope

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
 
   def index
     @q = Post.order(created_at: :desc).ransack(params[:q])
-    @posts = @q.result(distinct: true)
-    @new_posts = Post.order(created_at: :desc).limit(5)
+    @posts = @q.result.page(params[:page]).per(2)
+    @new_posts = Post.find_newest_article
     @author = Author.first
   end
 
