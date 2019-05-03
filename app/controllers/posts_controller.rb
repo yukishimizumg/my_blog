a3072c0181aebd24a41caab384c7af5b6ee909ad
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-05-03 11:58:03 +0900

    リファクタリング完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
 
