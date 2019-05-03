2ccb800b17105c12f2233ac90f88b702fe85ed9a
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-05-03 12:27:30 +0900

    検索機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.order(created_at: :desc)
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result(distinct: true)
     @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
